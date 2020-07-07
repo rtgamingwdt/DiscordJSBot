49c6e8d535076ff828054e881d4b74f85a1fbadc
rtgamingwdt <46906497+rtgamingwdt@users.noreply.github.com>
2020-07-07 15:10:48 -0400

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js');
-const botsettings = require('./botsettings.json');
-
-const bot = new Discord.Client({disableEveryone: true});
-
-bot.login(botsettings.token);
